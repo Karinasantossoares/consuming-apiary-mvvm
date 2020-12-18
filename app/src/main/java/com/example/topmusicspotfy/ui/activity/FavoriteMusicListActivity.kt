2b9a19235105921b6558a81fbca6000bebc15f85
Karinasantos <karina.bsb.x@hotmail.com>
2020-12-17 23:53:52 -0300

    second commit

---
+++ app/src/main/java/com/example/topmusicspotfy/ui/activity/FavoriteMusicListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.topmusicspotfy.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListFavoriteMusicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_favorite_music)
+    }
+}
